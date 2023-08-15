931915af72eb7b76ab22b09e4a873115d49abbe6
Aryan Sethi <46134586+neohacker18@users.noreply.github.com>
2023-08-15 21:46:31 +0530

    changes

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,14 @@
 const Pool = require("pg").Pool;
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 const pool = new Pool({
-  user: "notemaker",
-  password: "Nhn7Iht6MVwqVbUrOQ5CWAJYRfEb0gvK",
-  host: "dpg-cjdpn3rbq8nc73b6vsng-a.singapore-postgres.render.com",
+  user: process.env.POOL_USER,
+  password: process.env.POOL_PASSWORD,
+  host: process.env.POOL_HOST,
   port: 5432,
-  database: "tododb_m5my",
+  database: process.env.POOL_DB,
   ssl: {
     rejectUnauthorized: false
   }
