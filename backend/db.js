1f39fbb9deb48a5b5ae2e7722c280d333dea72d5
Aryan Sethi <46134586+neohacker18@users.noreply.github.com>
2023-08-15 22:30:32 +0530

    change

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,6 @@
 const Pool = require("pg").Pool;
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-
 
 const pool = new Pool({
   user: process.env.POOL_USER,
@@ -9,6 +8,9 @@ const pool = new Pool({
   host: process.env.POOL_HOST,
   port: 5432,
   database: process.env.POOL_DB,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-module.exports=pool;
+module.exports = pool;
